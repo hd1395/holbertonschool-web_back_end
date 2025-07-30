35c64195595099232da2722a3863c50d0c55c525
hamad <hamadsocial75@gmail.com>
2025-07-30 23:59:50 +0300

    fix an issue in 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ async generator yielding random numbers
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Yields a random float between 0 and 10.
     """
