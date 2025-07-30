cb22acbc0711ce8522dcb0a9d956b5eec64e4284
hamad <hamadsocial75@gmail.com>
2025-07-30 19:50:03 +0300

    add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float n and returns the
+    floor of the float as an integer.
+    """
+    return int(math.floor(n))
