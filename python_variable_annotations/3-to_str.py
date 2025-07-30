a5381d82c070bb27e3ead51ce053ae2fc9fe6760
hamad <hamadsocial75@gmail.com>
2025-07-30 19:53:27 +0300

    add 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float n and returns its string representation.
+    """
+    return str(n)
