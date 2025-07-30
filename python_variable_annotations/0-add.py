aaecd01fb245f063734b235f09cfe14562ca94c6
hamad <hamadsocial75@gmail.com>
2025-07-30 19:43:08 +0300

    add comment to 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-def add(a: float, b: float) -> float:
-    """
+"""
     This function takes two float arguments and returns their sum as a float.
-    """
+ """
+
+
+def add(a: float, b: float) -> float:
     return a + b
