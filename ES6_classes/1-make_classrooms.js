e04b15f994d9bac56a304f18008b9636562cd6eb
hamad <hamadsocial75@gmail.com>
2025-08-20 21:10:20 +0300

    fix lint issues

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 // 1-make_classrooms.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
