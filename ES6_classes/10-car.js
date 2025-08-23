e001bfd0b525416d833c25a2dbf880a716f4d8b3
hamad <hamadsocial75@gmail.com>
2025-08-23 19:40:27 +0300

    fix 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,31 +1,16 @@
-const BRAND = Symbol('brand');
-const MOTOR = Symbol('motor');
-const COLOR = Symbol('color');
-
 export default class Car {
   constructor(brand, motor, color) {
-    this[BRAND] = brand;
-    this[MOTOR] = motor;
-    this[COLOR] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
-  // Getter for brand
-  get brand() {
-    return this[BRAND];
+  static get [Symbol.species]() {
+    return this;
   }
 
-  // Getter for motor
-  get motor() {
-    return this[MOTOR];
-  }
-
-  // Getter for color
-  get color() {
-    return this[COLOR];
-  }
-
-  // Clone method that returns a new instance
   cloneCar() {
-    return new Car(this[BRAND], this[MOTOR], this[COLOR]);
+    const C = this.constructor[Symbol.species];
+    return new C();
   }
 }
