e3559213f438f00a66d4b0ba750229306e993703
hamad <hamadsocial75@gmail.com>
2025-08-23 18:09:00 +0300

    add 8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,26 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Getter for size
+  get size() {
+    return this._size;
+  }
+
+  // Getter for location
+  get location() {
+    return this._location;
+  }
+
+  // Cast to Number returns the size
+  valueOf() {
+    return this._size;
+  }
+
+  // Cast to String returns the location
+  toString() {
+    return this._location;
+  }
+}
