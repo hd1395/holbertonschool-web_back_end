49f2b8f03b29f232a9170dcb7a92329bf024cbb7
hamad <hamadsocial75@gmail.com>
2025-08-23 15:06:37 +0300

    add 5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+    /**
+     * @param {number} sqft The square footage of the building.
+     */
+    constructor(sqft) {
+        if (this.constructor === Building) {
+            throw new Error("Building is an abstract class and cannot be instantiated directly.");
+        }
+        this._sqft = sqft;
+
+        if (this.evacuationWarningMessage === undefined) {
+            throw new Error("Class extending Building must override evacuationWarningMessage");
+        }
+    }
+
+    /**
+     * Getter for the square footage.
+     * @returns {number} The square footage.
+     */
+    get sqft() {
+        return this._sqft;
+    }
+}
