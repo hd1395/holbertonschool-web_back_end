9c5be879b6b86ad3b32c303cd573af8fd0a5dcd7
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-08-25 17:48:41 +0300

    Create 4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+        return [];
+    }
+    
+    return students
+        .filter(student => student.location === city)
+        .map(student => {
+            const grade = newGrades.find(grade => grade.studentId === student.id);
+            return {
+                id: student.id,
+                firstName: student.firstName,
+                location: student.location,
+                grade: grade ? grade.grade : 'N/A'
+            };
+        });
+}
