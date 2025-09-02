7ac1c36ae3e26e9bcddf31780d081d674e0b0896
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-09-02 08:55:14 +0000

    add 1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+// 1-stdin.js
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.on('SIGINT', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
