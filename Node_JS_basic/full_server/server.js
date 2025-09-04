395b378c9ce8c545e532209c6eb1d9756b8650a3
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-09-04 13:38:15 +0000

    fix import in server

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import routes from './routes/index.js';
+import router from './routes/index.js';
 
 const app = express();
 const port = 1245;
 
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port);
 
