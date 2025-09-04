3fd54ec46b6b1ee4b7147e6d6a1f76717e125195
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-09-04 13:35:27 +0000

    revert to an older version

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -11,20 +11,4 @@ router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-export default function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
-
-  router.get('/', (req, res) => {
-    AppController.getHomepage(req, res);
-  });
-
-  router.get('/students', (req, res) => {
-    StudentsController.getAllStudents(req, res, process.argv[2]);
-  });
-
-  router.get('/students/:major', (req, res) => {
-    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
-  });
-}
-
+export default router;
