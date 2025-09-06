88c7e10eddaed162b64a4861a9e34e444f184777
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-09-06 21:19:21 +0300

    Update index.js

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import AppController from '../controllers/AppController.js';
-import StudentsController from '../controllers/StudentsController.js';
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
 
 const router = express.Router();
 
-// Root route
 router.get('/', AppController.getHomepage);
-
-// Students routes
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-export default router;
+module.exports = router;
