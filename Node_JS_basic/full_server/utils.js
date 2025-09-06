0f4ce52a5bfb0e60a72644b9baf1c021c54f0866
Hamad Dahash <48245047+hd1395@users.noreply.github.com>
2025-09-06 21:15:49 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,25 +1,26 @@
+const fs = require('fs');
 
-import fs from 'fs';
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-export function readDatabase(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = {};
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (!students[field]) {
+        students[field] = [];
       }
-
-      const lines = data.trim().split('\n').slice(1); // skip header
-      const fields = {};
-
-      for (const line of lines) {
-        const [firstname, , , field] = line.split(',');
-
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstname);
-      }
-
-      resolve(fields);
+      students[field].push(firstname);
     });
+
+    resolve(students);
   });
-}
+});
+
+module.exports = readDatabase;
